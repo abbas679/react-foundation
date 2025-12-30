518b8163e1a02abb6346eeaa73c795d42d80230e
abbas679 <tahir76422@gmail.com>
2025-12-30 19:21:41 +0500

    Better use of props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import UserCard from "./components/UserCard";
 
 function App() {
   return (
     <>
       <Header title="Dashboard" />
-
+      <UserCard name="Tahir" role="Developer" />
+      <UserCard name="Taheer" role="Owner" />
       <Footer />
     </>
   );
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,10 @@
+function UserCard({ name, role }) {
+  return (
+    <div>
+      <p>Name: {name}</p>
+      <p>Role: {role}</p>
+    </div>
+  );
+}
+
+export default UserCard;
